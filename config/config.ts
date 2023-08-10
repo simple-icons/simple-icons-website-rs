5c88e3655d1644a8a1900d7e87bee04dcae5c594
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2023-08-10 20:59:32 +0200

    Refactor configuration and scripts

---
+++ config/config.ts
@@ -0,0 +1,33 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const parseRustConfig = async (env = 'prod') => {
+  const rustConfigFilePath = path.resolve(__dirname, `settings.${env}.json`);
+  const rustConfigFile = await fs.readFile(rustConfigFilePath, 'utf8');
+  return JSON.parse(rustConfigFile);
+};
+
+export default await parseRustConfig();
+
+export const getGithubToken = async () => {
+  const envFilePath = path.resolve(__dirname, '..', '.env');
+  const envFileExists = await fs
+    .access(envFilePath)
+    .then(() => true)
+    .catch(() => false);
+  if (!envFileExists) {
+    throw new Error(`.env file not found at ${envFilePath}`);
+  }
+
+  const envFile = await fs.readFile(envFilePath, 'utf8');
+  for (const line of envFile.split('\n')) {
+    const [key, value] = line.split('=');
+    if (key.trim() === 'GITHUB_TOKEN') {
+      return value.split('"')[1].trim();
+    }
+  }
+  throw new Error(`GITHUB_TOKEN not found in .env file at ${envFilePath}`);
+};
