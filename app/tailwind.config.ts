24b1adcad81d6e08d77d90ac8d2f27c034adb802
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2023-10-17 07:41:23 +0200

    Refactor with components

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -34,7 +34,6 @@ export default {
           ...defaultTheme.fontFamily.mono,
         ],
         sans: [
-          'Roboto',
           '"Open Sans"',
           'Arial',
           'Helvetica',
--- components/src/preview/mod.rs
+++ components/src/preview/mod.rs
@@ -1,37 +1,18 @@
 use i18n::move_tr;
 use leptos::{html::Input, *};
-use macros::{
-    get_number_of_icons, simple_icon_svg_content, simple_icon_svg_path,
-};
+use macros::{get_number_of_icons, simple_icon_svg_path};
 use simple_icons::{color, sdk::title_to_slug};
 
-fn on_brand_input(input_ref: NodeRef<Input>) {
-    let input = input_ref.get().unwrap();
-    let value = input.value();
-    let info = document()
-        .get_element_by_id("preview-info")
-        .unwrap()
-        .children();
-    info.get_with_index(0)
-        .unwrap()
-        .set_inner_html(&format!("{} Preview", value));
-    info.get_with_index(1)
-        .unwrap()
-        .set_inner_html(&format!("{}.svg", title_to_slug(&value)));
-    info.get_with_index(2)
-        .unwrap()
-        .set_inner_html(&format!("Brand: {}", value));
+fn initial_brand_value() -> String {
+    "Simple Icons".to_string()
 }
 
-#[derive(Copy, Clone)]
-struct OnColorInputOptions {
-    update_body_bg: bool,
+fn initial_color() -> String {
+    "111111".to_string()
 }
 
-impl OnColorInputOptions {
-    fn new(update_body_bg: bool) -> Self {
-        Self { update_body_bg }
-    }
+fn initial_path() -> String {
+    simple_icon_svg_path!("simpleicons").to_string()
 }
 
 fn is_valid_hex_color(value: &str) -> bool {
@@ -47,67 +28,6 @@ fn is_valid_hex_color(value: &str) -> bool {
     true
 }
 
-fn normalize_hex_color(value: &str) -> (String, String) {
-    let original_value = value.to_uppercase().replace('#', "");
-    let mut result = original_value.clone();
-    if result.len() == 3 {
-        let mut normalized = String::with_capacity(6);
-        for c in result.chars() {
-            normalized.push(c);
-            normalized.push(c);
-        }
-        result = normalized;
-    }
-    (result, original_value)
-}
-
-fn on_color_input(input_ref: NodeRef<Input>, opts: OnColorInputOptions) {
-    let input = input_ref.get().unwrap();
-    let (value, original_value) = normalize_hex_color(&input.value());
-
-    if !is_valid_hex_color(&value) {
-        input.class_list().add_1("invalid").unwrap();
-    } else {
-        input.class_list().remove_1("invalid").unwrap();
-        if opts.update_body_bg {
-            let rect = document().get_element_by_id("preview-body-bg").unwrap();
-            rect.set_attribute("fill", &format!("#{}", value)).unwrap();
-
-            let is_light_hex = color::is_relatively_light_icon_hex(&value);
-
-            let paths = document().get_elements_by_class_name("preview-path");
-            for i in 0..paths.length() {
-                let path = paths.get_with_index(i).unwrap();
-                path.set_attribute(
-                    "fill",
-                    if is_light_hex { "black" } else { "white" },
-                )
-                .unwrap();
-            }
-
-            let preview_info_container = document()
-                .get_element_by_id("preview-info")
-                .unwrap()
-                .children()
-                .get_with_index(3)
-                .unwrap();
-            preview_info_container
-                .set_inner_html(&format!("{}: #{}", "Color", value));
-        }
-    }
-    input.set_value(&original_value);
-}
-
-fn on_path_input(input_ref: NodeRef<Input>) {
-    let input = input_ref.get().unwrap();
-    let value = input.value();
-    let paths = document().get_elements_by_class_name("preview-path");
-    for i in 0..paths.length() {
-        let path = paths.get_with_index(i).unwrap();
-        path.set_attribute("d", &value).unwrap();
-    }
-}
-
 fn badge_url(
     slug: &str,
     color: &str,
@@ -121,24 +41,29 @@ fn badge_url(
         colored_svg = svg.replace("<path ", &replacement);
     }
     format!(
-        "https://img.shields.io/badge/{}-preview-{}.svg?style={}&color={}&logo=data:image/svg%2bxml;base64,{}",
+        "https://img.shields.io/badge/{}-preview-{}.svg?style={}&logo=data:image/svg%2bxml;base64,{}",
         slug,
         color,
         style,
-        "green",
         window().btoa(&colored_svg).unwrap(),
     )
 }
 
 #[component]
-fn PreviewBadge(
-    slug: &'static str,
-    color: &'static str,
-    svg: &'static str,
+fn PreviewBadge<S, C, G, V>(
+    slug: S,
+    color: C,
+    svg: G,
     style: &'static str,
-    svg_color: &'static str,
-) -> impl IntoView {
-    let url = badge_url(slug, color, svg, style, svg_color);
+    svg_color: V,
+) -> impl IntoView
+where
+    S: Fn() -> String + 'static,
+    C: Fn() -> String + 'static,
+    V: Fn() -> String + 'static,
+    G: Fn() -> String + 'static,
+{
+    let url = badge_url(&slug(), &color(), &svg(), style, &svg_color());
     view! {
         <div>
             <img src=url/>
@@ -158,6 +83,8 @@ impl PreviewButtonSvgPath {
     }
 }
 
+fn update_canvas() {}
+
 #[component]
 fn PreviewButton(
     svg_path: PreviewButtonSvgPath,
@@ -175,18 +102,28 @@ fn PreviewButton(
 
 #[component]
 pub fn PreviewBox() -> impl IntoView {
+    let (brand, set_brand) = create_signal(initial_brand_value());
     let brand_input_ref = create_node_ref::<Input>();
-
+    let (color, set_color) = create_signal(initial_color());
     let color_input_ref = create_node_ref::<Input>();
-    color_input_ref.on_load(move |input| {
-        input.set_value("111111");
-        on_color_input(color_input_ref, OnColorInputOptions::new(false))
-    });
-    let on_color_input_opts = OnColorInputOptions::new(true);
-
+    let (path, set_path) = create_signal(initial_path());
     let path_input_ref = create_node_ref::<Input>();
 
-    let simpleicons_svg_content = simple_icon_svg_content!("simpleicons");
+    fn contrast_color_for(hex: &str) -> String {
+        let is_light_hex = color::is_relatively_light_icon_hex(hex);
+        if is_light_hex { "black" } else { "white" }.to_string()
+    }
+
+    fn build_svg(path: &str, fill: Option<String>) -> String {
+        format!(
+            "<svg role=\"img\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"{}\"{}/></svg>",
+            path,
+            match fill {
+                Some(fill) => format!(" fill=\"{}\"", fill),
+                None => "".to_string(),
+            }
+        )
+    }
 
     view! {
         <div class="preview">
@@ -199,9 +136,13 @@ pub fn PreviewBox() -> impl IntoView {
                         class="mr-7"
                         style="width:524px"
                         name="preview-brand"
-                        value="Simple Icons"
-                        on:input=move |_| { on_brand_input(brand_input_ref) }
+                        value=brand
+                        on:input=move |_| {
+                            set_brand(brand_input_ref.get().unwrap().value());
+                            update_canvas();
+                        }
                     />
+
                 </div>
                 <div class="preview-input-group">
                     <label for="preview-color">Color</label>
@@ -210,7 +151,18 @@ pub fn PreviewBox() -> impl IntoView {
                         type="text"
                         style="width:68px"
                         name="preview-color"
-                        on:input=move |_| { on_color_input(color_input_ref, on_color_input_opts) }
+                        value=color
+                        prop:value=color
+                        on:input=move |_| {
+                            let input = color_input_ref.get().unwrap();
+                            let normalized_value = input.value().to_uppercase().replace('#', "");
+                            input.set_value(&normalized_value);
+                            set_color(normalized_value);
+                            update_canvas();
+                        }
+
+                        class:invalid=move || !is_valid_hex_color(&color())
+                        maxlength=6
                     />
 
                 </div>
@@ -223,7 +175,10 @@ pub fn PreviewBox() -> impl IntoView {
                     style="width:682px"
                     name="preview-path"
                     value=simple_icon_svg_path!("simpleicons")
-                    on:input=move |_| { on_path_input(path_input_ref) }
+                    on:input=move |_| {
+                        set_path(path_input_ref.get().unwrap().value());
+                        update_canvas();
+                    }
                 />
 
             </div>
@@ -237,9 +192,8 @@ pub fn PreviewBox() -> impl IntoView {
                     class="pt-3"
                 >
                     <rect
-                        fill="#111111"
+                        fill=move || format!("#{}", color())
                         height="420"
-                        id="preview-body-bg"
                         rx="10"
                         ry="10"
                         width="100%"
@@ -248,60 +202,53 @@ pub fn PreviewBox() -> impl IntoView {
                         class="pt-3"
                     ></rect>
                     <svg viewBox="0 0 24 24" width="24" height="24" x="18" y="20">
-                        <path
-                            d=simple_icon_svg_path!("simpleicons")
-                            class="preview-path"
-                            fill="white"
-                        ></path>
+                        <path d=move || path() fill=move || contrast_color_for(&color())></path>
                     </svg>
                     <svg viewBox="0 0 24 24" width="80" height="80" x="70" y="20">
-                        <path
-                            d=simple_icon_svg_path!("simpleicons")
-                            class="preview-path"
-                            fill="white"
-                        ></path>
+                        <path d=move || path() fill=move || contrast_color_for(&color())></path>
                     </svg>
                     <svg viewBox="0 0 24 24" width="138" height="138" x="174" y="20">
-                        <path
-                            d=simple_icon_svg_path!("simpleicons")
-                            class="preview-path"
-                            fill="white"
-                        ></path>
+                        <path d=move || path() fill=move || contrast_color_for(&color())></path>
                     </svg>
                     <svg viewBox="0 0 24 24" width="375" height="375" x="350" y="20">
-                        <path
-                            d=simple_icon_svg_path!("simpleicons")
-                            class="preview-path"
-                            fill="white"
-                        ></path>
+                        <path d=move || path() fill=move || contrast_color_for(&color())></path>
                     </svg>
 
-                    <g id="preview-info" transform="translate(21,235)">
-                        <text fill="white" font-size="25">
-                            Simple Icons Preview
+                    <g transform="translate(21,235)">
+                        <text fill=move || contrast_color_for(&color()) font-size="25">
+                            {move || format!("{} Preview", brand())}
                         </text>
-                        <text fill="white" font-size="17" y="25">
-                            simpleicons.svg
+                        <text fill=move || contrast_color_for(&color()) font-size="17" y="25">
+                            {move || format!("{}.svg", title_to_slug(&brand()))}
                         </text>
-                        <text fill="white" font-size="16" y="60">
-                            Brand: Simple Icons
+                        <text fill=move || contrast_color_for(&color()) font-size="16" y="60">
+                            {move || format!("Brand: {}", brand())}
                         </text>
-                        <text fill="white" font-size="16" y="80">
-                            Color: #111111
+                        <text fill=move || contrast_color_for(&color()) font-size="16" y="80">
+                            {move || format!("Color: #{}", color())}
                         </text>
 
                         <g transform="translate(3, 142)">
                             <svg viewBox="0 0 24 24" width="24" height="24">
                                 <path
                                     d=simple_icon_svg_path!("simpleicons")
-                                    class="preview-path"
-                                    fill="white"
+                                    fill=move || contrast_color_for(&color())
                                 ></path>
                             </svg>
-                            <text fill="white" x="30" y="7" font-size="12">
+                            <text
+                                fill=move || contrast_color_for(&color())
+                                x="30"
+                                y="7"
+                                font-size="12"
+                            >
                                 {format!("{} Free SVG brand icons", get_number_of_icons!())}
                             </text>
-                            <text fill="white" x="30" y="25" font-size="12">
+                            <text
+                                fill=move || contrast_color_for(&color())
+                                x="30"
+                                y="25"
+                                font-size="12"
+                            >
                                 available at simpleicons.org
                             </text>
                         </g>
@@ -311,60 +258,60 @@ pub fn PreviewBox() -> impl IntoView {
             </figure>
             <div class="preview-badges">
                 <PreviewBadge
-                    slug="simpleicons"
-                    color="111111"
-                    svg=simpleicons_svg_content
+                    slug=move || title_to_slug(&brand())
+                    color=color
+                    svg=move || build_svg(&path(), None)
                     style="flat"
-                    svg_color="white"
+                    svg_color=move || "white".to_string()
                 />
                 <PreviewBadge
-                    slug="simpleicons"
-                    color="111111"
-                    svg=simpleicons_svg_content
+                    slug=move || title_to_slug(&brand())
+                    color=color
+                    svg=move || build_svg(&path(), None)
                     style="plastic"
-                    svg_color="white"
+                    svg_color=move || "white".to_string()
                 />
                 <PreviewBadge
-                    slug="simpleicons"
-                    color="111111"
-                    svg=simpleicons_svg_content
+                    slug=move || title_to_slug(&brand())
+                    color=color
+                    svg=move || build_svg(&path(), None)
                     style="for-the-badge"
-                    svg_color="white"
+                    svg_color=move || "white".to_string()
                 />
                 <PreviewBadge
-                    slug="simpleicons"
-                    color="111111"
-                    svg=simpleicons_svg_content
+                    slug=move || title_to_slug(&brand())
+                    color=color
+                    svg=move || build_svg(&path(), None)
                     style="flat-square"
-                    svg_color="white"
+                    svg_color=move || "white".to_string()
                 />
                 <PreviewBadge
-                    slug="simpleicons"
-                    color="111111"
-                    svg=simpleicons_svg_content
+                    slug=move || title_to_slug(&brand())
+                    color=color
+                    svg=move || build_svg(&path(), None)
                     style="flat"
-                    svg_color="111111"
+                    svg_color=color
                 />
                 <PreviewBadge
-                    slug="simpleicons"
-                    color="111111"
-                    svg=simpleicons_svg_content
+                    slug=move || title_to_slug(&brand())
+                    color=color
+                    svg=move || build_svg(&path(), None)
                     style="plastic"
-                    svg_color="111111"
+                    svg_color=color
                 />
                 <PreviewBadge
-                    slug="simpleicons"
-                    color="111111"
-                    svg=simpleicons_svg_content
+                    slug=move || title_to_slug(&brand())
+                    color=color
+                    svg=move || build_svg(&path(), None)
                     style="for-the-badge"
-                    svg_color="111111"
+                    svg_color=color
                 />
                 <PreviewBadge
-                    slug="simpleicons"
-                    color="111111"
-                    svg=simpleicons_svg_content
+                    slug=move || title_to_slug(&brand())
+                    color=color
+                    svg=move || build_svg(&path(), None)
                     style="social"
-                    svg_color="black"
+                    svg_color=move || "black".to_string()
                 />
             </div>
             <div class="preview-buttons">
