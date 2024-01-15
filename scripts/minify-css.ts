5aab1b4cc8884cf9507bb010b47407541bcc9843
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-01-15 22:49:31 +0100

    Minor change

--- scripts/minify-css.ts
+++ scripts/minify-css.ts
@@ -4,16 +4,17 @@ import { transform } from 'lightningcss';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const PUBLIC_ASSETS = path.join(__dirname, '../app/public/assets');
 
 const getCssFiles = async () => {
-  const files = await fs.readdir(path.join(__dirname, '../app/public/assets'));
+  const files = await fs.readdir(PUBLIC_ASSETS);
   return files.filter((file) => file.endsWith('.css'));
 };
 
 const minifyCss = async () => {
   const cssFiles = await getCssFiles();
   for (const cssFile of cssFiles) {
-    const cssFilePath = path.join(__dirname, '../app/public/assets', cssFile);
+    const cssFilePath = path.join(PUBLIC_ASSETS, cssFile);
 
     const cssFileContent = await fs.readFile(cssFilePath, 'utf-8');
     const minifiedCss = transform({
