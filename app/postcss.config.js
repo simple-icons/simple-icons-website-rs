34c5511dad4bdbe62803c6b5c6f8a66914aae0b0
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2025-04-23 22:19:21 +0200

    Migrate from ESLint to XO

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -1,8 +1,14 @@
+/**
+ * @file PostCSS configuration file.
+ */
+
 import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
 import postcssImport from 'postcss-import';
+import tailwindcss from 'tailwindcss';
 
 /** @type {import('postcss-load-config').Config} */
-export default {
-  plugins: [autoprefixer, tailwindcss, postcssImport],
+const config = {
+	plugins: [autoprefixer, tailwindcss, postcssImport],
 };
+
+export default config;
